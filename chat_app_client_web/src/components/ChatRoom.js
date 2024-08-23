e7b6d541b897244fcc7ff1eb798b592b2133f448
Phan Van Minh <21522345@gm.uit.edu.vn>
2024-08-24 01:40:30 +0700

    upload image to web

--- chat_app_client_web/src/components/ChatRoom.js
+++ chat_app_client_web/src/components/ChatRoom.js
@@ -1,10 +1,12 @@
-import { useEffect, useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import MessageContainer from "./MessageContainer";
 
 function ChatRoom({ userId, messages, sendMessage }) {
   const dummy = useRef();
 
   const [message, setMessage] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
+  const fileInputRef = useRef();
 
   // useEffect để tự động cuộn xuống cuối khi có tin nhắn mới
   useEffect(() => {
@@ -15,7 +17,24 @@ function ChatRoom({ userId, messages, sendMessage }) {
     e.preventDefault();
     sendMessage(message);
     setMessage("");
+    setSelectedImage(null);
     dummy.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setSelectedImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const removeImage = () => {
+    setSelectedImage(null);
+    fileInputRef.current.value = null; // Reset giá trị input file
   };
 
   return (
@@ -26,6 +45,29 @@ function ChatRoom({ userId, messages, sendMessage }) {
       </main>
 
       <form className="message-form" onSubmit={handleSubmit}>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          style={{ display: "none" }}
+          id="image-upload"
+          ref={fileInputRef}
+        />
+        <label htmlFor="image-upload" className="image-upload-label">
+          📷
+        </label>
+        {selectedImage && (
+          <div className="image-preview-container">
+            <img src={selectedImage} alt="Preview" className="image-preview" />
+            <button
+              type="button"
+              className="remove-image-button"
+              onClick={removeImage}
+            >
+              &times;
+            </button>
+          </div>
+        )}
         <input
           value={message}
           onChange={(e) => setMessage(e.target.value)}
