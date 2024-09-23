48af32209fbc842640507134a186876bd2138908
Phan Van Minh <21522345@gm.uit.edu.vn>
2024-09-23 15:05:37 +0700

    Add Page Size

--- chat_app_client_web/src/constants/ApiUrl.js
+++ chat_app_client_web/src/constants/ApiUrl.js
@@ -8,7 +8,7 @@ export const API_CHAT_URL = SERVER_URL + CHAT_ENDPOINT;
 
 export const GetAPIMessagesUrl = (roomId) => {
   if (roomId.trim()) {
-    return `${SERVER_URL}/api/messages?filterOn=room_id&filterQuery=${roomId}&sortBy=create_date&isAscending=false&pageNumber=1&pageSize=20`;
+    return `${SERVER_URL}/api/messages?filterOn=room_id&filterQuery=${roomId}&sortBy=create_date&isAscending=false&pageNumber=1&pageSize=50`;
   }
   return `${SERVER_URL}/api/messages?filterOn=room_id&sortBy=create_date&isAscending=false&pageNumber=1&pageSize=20`;
 };
